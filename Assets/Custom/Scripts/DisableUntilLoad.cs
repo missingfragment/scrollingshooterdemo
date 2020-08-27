9d50e84c578830caa6cac6bbb849d2631896aacc
missingfragment <phyrophantom@gmail.com>
2020-08-27 14:21:12 -0500

    Level now preloads instead of loading after Start was pressed.

---
+++ Assets/Custom/Scripts/DisableUntilLoad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableUntilLoad : MonoBehaviour
+{
+    [SerializeField]
+    private ScenePreloader preloader = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (preloader == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        gameObject.SetActive(false);
+        preloader.SceneReady += OnSceneReady;
+    }
+
+    private void OnSceneReady()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Custom/Scripts/ScenePreloader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ScenePreloader : MonoBehaviour
+{
+    public event Action SceneReady;
+
+    [SerializeField]
+    private int sceneBuildIndex;
+
+    [SerializeField]
+    private TMP_Text loadingText;
+
+    private AsyncOperation asyncLoad;
+
+    public float LoadProgress { get; private set; } = 0f;
+
+    private IEnumerator Start()
+    {
+        asyncLoad = SceneManager.LoadSceneAsync(sceneBuildIndex);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            LoadProgress = asyncLoad.progress;
+            if (LoadProgress >= 0.9f)
+            {
+                SceneReady?.Invoke();
+                if (loadingText != null)
+                {
+                    loadingText.gameObject.SetActive(false);
+                }
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    public void ActivateScene()
+    {
+        if (asyncLoad.progress >= 0.9f)
+        {
+            asyncLoad.allowSceneActivation = true;
+        }
+    }
+
+}
